2eb0c845e77d9e833be07a9c6200a5737a30a0d0
Martin Larralde <martin.larralde@embl.de>
2023-03-25 14:31:09 +0100

    Add `render` command to show the predicted probabilities as a `rich` tree

--- conch/cli/__init__.py
+++ conch/cli/__init__.py
@@ -1,9 +1,10 @@
 import argparse
 from typing import Optional
 
+import torch
 from rich.console import Console
 
-from . import train, predict
+from . import train, predict, render
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -16,15 +17,21 @@ def build_parser() -> argparse.ArgumentParser:
     train.configure_parser(parser_train)
     parser_predict = commands.add_parser("predict")
     predict.configure_parser(parser_predict)
+    parser_render = commands.add_parser("render")
+    render.configure_parser(parser_render)
     
     return parser
 
 def run(console: Optional[Console] = None) -> int:
     if console is None:
         console = Console()
+    
     parser = build_parser()
     args = parser.parse_args()
+
     try:
+        if args.jobs is not None:
+            torch.set_num_threads(args.jobs)
         args.run(args, console)                
         return 0
     except Exception as err:
---
+++ conch/cli/render.py
@@ -0,0 +1,77 @@
+import argparse
+import pathlib
+from typing import List, Set, Iterable
+
+import anndata
+import numpy
+import rich.tree
+import rich.panel
+from rich.console import Console
+from torch_treecrf import TreeMatrix
+
+from ..predictor import ChemicalHierarchyPredictor
+
+
+def configure_parser(parser: argparse.ArgumentParser):
+    parser.add_argument("-m", "--model", required=True, type=pathlib.Path)
+    parser.add_argument("-i", "--input", required=True, type=pathlib.Path)
+    parser.set_defaults(run=run)
+
+
+def all_superclasses( classes: Iterable[int], hierarchy: TreeMatrix ) -> Set[int]:
+    superclasses = set()
+    classes = set(classes)
+    while classes:
+        i = classes.pop()
+        superclasses.add(i)
+        classes.update(j.item() for j in hierarchy.parents(i))
+        superclasses.update(j.item() for j in hierarchy.parents(i))
+    return superclasses
+
+
+def build_tree(
+    model: ChemicalHierarchyPredictor, 
+    bgc_index: int,
+    bgc_probas: numpy.ndarray, 
+) -> None:
+    # get probabilities and corresponding positive terms from ChemOnt
+    bgc_labels = bgc_probas > 0.5
+    terms = { j for j in range(model.n_labels) if bgc_probas[j] > 0.5 }
+    whitelist = all_superclasses(terms, model.hierarchy)
+    # render a tree structure with rich
+    def render(i, tree, whitelist):
+        term_id = model.labels.index[i]
+        term_name = model.labels.name[i]
+        label = f"[bold blue]{term_id}[/] ([green]{term_name}[/]): [bold cyan]{bgc_probas[i]:.3f}[/]"
+        subtree = tree.add(label, highlight=False)
+        for j in model.hierarchy.children(i):
+            j = j.item()
+            if j in whitelist:
+                render(j, subtree, whitelist)
+    roots = [
+        i 
+        for i in range(model.n_labels) 
+        if not len(model.hierarchy.parents(i)) 
+        and bgc_probas[i] > 0.5
+    ]
+    tree = rich.tree.Tree(".", hide_root=True)
+    for root in roots:
+        render(root, tree, whitelist=whitelist)
+    return tree
+
+
+def run(args: argparse.Namespace, console: Console) -> int:
+    # load model
+    console.print(f"[bold blue]{'Loading':>12}[/] trained model from {str(args.model)!r}")
+    with open(args.model, "rb") as src:
+        model = ChemicalHierarchyPredictor.load(src)
+
+    # load predictions
+    console.print(f"[bold blue]{'Loading':>12}[/] probability predictions from {str(args.input)!r}")
+    predictions = anndata.read(args.input)
+
+    # render tree
+    for bgc_index in range(predictions.n_obs):
+        tree = build_tree(model, bgc_index, predictions.X[bgc_index].toarray()[0])
+        panel = rich.panel.Panel(tree, title=predictions.obs_names[bgc_index])
+        console.print(panel)
