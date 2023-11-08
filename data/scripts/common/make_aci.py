8b4849c5b51d8cf199a4df598f459a662e412364
Martin Larralde <martin.larralde@embl.de>
2023-11-08 17:34:59 +0100

    Add script to compute average protein identity between clusters

---
+++ data/scripts/common/make_aci.py
@@ -0,0 +1,176 @@
+import argparse
+import contextlib
+import statistics
+import io
+import tarfile
+import os
+import sys
+import tempfile
+import subprocess
+import multiprocessing.pool
+
+import anndata
+import iocursor
+import gb_io
+import numpy
+import rich.panel
+import rich.progress
+import scipy.sparse
+import pandas
+import pyrodigal
+import Bio.SeqIO
+
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, "..", "..", "..", "..")))
+import conch.orf
+from conch.model import ClusterSequence
+from conch.cli._common import find_proteins
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-q", "--query", required=True)
+parser.add_argument("-t", "--target", required=True)
+parser.add_argument("-o", "--output", required=True)
+parser.add_argument("-j", "--jobs", type=int)
+args = parser.parse_args()
+
+# run many-to-many comparison
+with tempfile.TemporaryDirectory() as dst:
+    
+    with rich.progress.Progress() as progress:
+        # load query records
+        with progress.open(args.query, "r", description=f"[bold blue]{'Loading':>12}[/]") as src:
+            query_records = {
+                record.name:ClusterSequence(record)
+                for record in gb_io.iter(src)
+            }
+            query_ids = sorted(query_records)
+            query_indices = { name:i for i, name in enumerate(query_ids) }
+        # load target records
+        with progress.open(args.target, "r", description=f"[bold blue]{'Loading':>12}[/]") as src:
+            target_records = {
+                record.name:ClusterSequence(record)
+                for record in gb_io.iter(src)
+            }
+            target_ids = sorted(target_records)
+            target_indices = { name:i for i, name in enumerate(target_ids) }
+
+    # find genes
+    progress.console.print(f"[bold blue]{'Finding':>12}[/] genes in input records")
+    orf_finder = conch.orf.PyrodigalFinder(cpus=args.jobs)
+    query_genes = find_proteins(list(query_records.values()), orf_finder, progress.console)
+    target_genes = find_proteins(list(target_records.values()), orf_finder, progress.console)
+
+    # record protein length
+    protein_lengths = {
+        protein.id:len(protein.sequence)
+        for proteins in (query_genes, target_genes)
+        for protein in proteins
+    }
+
+    # write target genes
+    progress.console.print(f"[bold blue]{'Writing':>12}[/] protein sequences")
+    db_faa = os.path.join(dst, "db.faa")
+    with open(db_faa, "w") as f:
+        for protein in progress.track(target_genes, description=f"[bold blue]{'Writing':>12}[/]"):
+            f.write(f">{protein.id}\n")                
+            f.write(f"{protein.sequence.rstrip('*')}\n")                
+    
+    # make blastd
+    db_filename = os.path.join(dst, "db.db")
+    proc = subprocess.run([
+        "diamond", 
+        "makedb", 
+        "--in", 
+        db_faa, 
+        "--db",
+        db_filename,
+        "--tmpdir",
+        dst,
+        "--threads", 
+        str(args.jobs or os.cpu_count())
+    ], capture_output=True)
+    if proc.returncode != 0:
+        rich.print(rich.panel.Panel(proc.stderr.decode()))
+    proc.check_returncode()
+
+    # write query records
+    query_filename = os.path.join(dst, "query.faa")
+    with open(query_filename, "w") as f:
+        for protein in progress.track(query_genes, description=f"[bold blue]{'Writing':>12}[/]"):
+            f.write(f">{protein.id}\n")                
+            f.write(f"{protein.sequence.rstrip('*')}\n")                
+
+    # run BLASTn
+    proc = subprocess.run([
+        "diamond",
+        "blastp",
+        "--query",
+        query_filename,
+        "--db",
+        db_filename,
+        "--threads", 
+        str(args.jobs or os.cpu_count()),
+        "--outfmt",
+        "6",
+        "--max-target-seqs",
+        str(len(target_ids)),
+        "--tmpdir",
+        dst,
+    ], capture_output=True)
+    if proc.returncode != 0:
+        rich.print(rich.panel.Panel(proc.stderr.decode()))
+    proc.check_returncode()
+
+    hits = pandas.read_table(
+        iocursor.Cursor(proc.stdout),
+        comment="#",
+        header=None,
+        index_col=None,
+        names=[
+            "query_protein",
+            "target_protein",
+            "identity",
+            "alilen",
+            "mismatches",
+            "gapopens",
+            "qstart",
+            "qend",
+            "sstart",
+            "send",
+            "evalue",
+            "bitscore"
+        ]
+    )
+    hits["query_cluster"] = hits["query_protein"].str.rsplit("_", 1).str[0]
+    hits["target_cluster"] = hits["target_protein"].str.rsplit("_", 1).str[0]
+    hits["query_index"] = hits["query_cluster"].map(query_indices.__getitem__)
+    hits["target_index"] = hits["target_cluster"].map(target_indices.__getitem__)
+
+# compute identity
+identity = numpy.zeros((len(query_records), len(target_records)), dtype=numpy.float_)
+for row in hits.itertuples():
+    identity[row.query_index, row.target_index] += row.identity * row.alilen / 100.0
+
+# normalize by query length
+for i, query_id in enumerate(query_ids):
+    query_length = sum( 
+        len(protein.sequence) 
+        for protein in query_genes 
+        if protein.cluster.id == query_id
+    )
+    print(query_id, query_length)
+    identity[i] /= query_length
+
+# make distances symmetric
+identity = numpy.clip(identity, 0, 1)
+
+# generate annotated data
+data = anndata.AnnData(
+    dtype=numpy.float_,
+    X=scipy.sparse.csr_matrix(identity),
+    obs=pandas.DataFrame(index=query_ids),
+    var=pandas.DataFrame(index=target_ids),
+)
+
+# save annotated data
+os.makedirs(os.path.dirname(args.output), exist_ok=True)
+data.write(args.output)
--- data/scripts/common/make_ani.py
+++ data/scripts/common/make_ani.py
@@ -12,34 +12,42 @@ import scipy.sparse
 
 # get paths from command line
 parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--input", required=True)
+parser.add_argument("-q", "--query", required=True)
+parser.add_argument("-r", "--reference", required=True)
 parser.add_argument("-o", "--output", required=True)
 parser.add_argument("-s", "--similarity", type=float, default=0.5)
 args = parser.parse_args()
 
 # 
-names = []
+ref_names = []
 database = pyskani.Database(compression=10, marker_compression=50)
-with rich.progress.open(args.input, "rb", description=f"[bold blue]{'Loading':>12}[/] BGCs") as f:
+with rich.progress.open(args.reference, "rb", description=f"[bold blue]{'Sketching':>12}[/] reference BGCs") as f:
     for record in  gb_io.iter(f):
         database.sketch(record.name, record.sequence)
-        names.append(record.name)
+        ref_names.append(record.name)
 
-name_index = {x:i for i,x in enumerate(names)}
-X = scipy.sparse.dok_matrix(( len(names), len(names) ))
+# 
+query_names = []
+with rich.progress.open(args.query, "rb", description=f"[bold blue]{'Indexing':>12}[/] query BGCs") as f:
+    for record in  gb_io.iter(f):
+        query_names.append(record.name)
+
+query_name_index = {x:i for i,x in enumerate(query_names)}
+ref_name_index = {x:i for i,x in enumerate(ref_names)}
+X = scipy.sparse.dok_matrix(( len(query_names), len(ref_names) ))
 
 #
-with rich.progress.open(args.input, "rb", description=f"[bold blue]{'Querying':>12}[/] BGCs") as f:
+with rich.progress.open(args.query, "rb", description=f"[bold blue]{'Querying':>12}[/] BGCs") as f:
     for record in  gb_io.iter(f):
         for hit in database.query(record.name, record.sequence):
-            i = name_index[hit.query_name]
-            j = name_index[hit.reference_name]
+            i = query_name_index[hit.query_name]
+            j = ref_name_index[hit.reference_name]
             X[i, j] = hit.identity * max(hit.query_fraction, hit.reference_fraction)
 
 #
-group_set = disjoint_set.DisjointSet({ i:i for i in range(len(names)) })
-indices = itertools.combinations(range(len(names)), 2)
-total = len(names) * (len(names) - 1) / 2
+group_set = disjoint_set.DisjointSet({ i:i for i in range(len(query_names)) })
+indices = itertools.combinations(range(len(query_names)), 2)
+total = len(query_names) * (len(query_names) - 1) / 2
 for (i, j) in rich.progress.track(indices, total=total, description=f"[bold blue]{'Grouping':>12}[/]"):
     if X[i, j] > args.similarity:
         group_set.union(i, j)
@@ -47,6 +55,7 @@ n = sum(1 for _ in group_set.itersets())
 rich.print(f"[bold green]{'Built':>12}[/] {n} groups of clusters with >{args.similarity:.1%} local identity")
 
 rich.print(f"[bold blue]{'Writing':>12}[/] ANI table to {args.output!r}")
-obs = pandas.DataFrame(index=names, data=dict(groups=[group_set[i] for i in range(len(names))]))
-data = anndata.AnnData(obs=obs, var=obs, X=X.tocsr())
+obs = pandas.DataFrame(index=query_names, data=dict(groups=[group_set[i] for i in range(len(query_names))]))
+var = pandas.DataFrame(index=ref_names)
+data = anndata.AnnData(obs=obs, var=var, X=X.tocsr())
 data.write(args.output)
